d3d279d2ea3fe91e846b6308f0f037a19c347e44
Ian McEwen <mian@cyverse.org>
2017-07-24 16:02:33 -0700

    Upgrade clj-jargon for performance improvements

--- project.clj
+++ project.clj
@@ -34,7 +34,7 @@
                  [org.cyverse/clj-icat-direct "2.8.1"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
-                 [org.cyverse/clj-jargon "2.8.4"
+                 [org.cyverse/clj-jargon "2.8.5"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
                  [org.cyverse/clojure-commons "2.8.3"]
