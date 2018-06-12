de5447c1180d757817a07f565b733df77f35e4a7
Sarah Roberts <sarahr@cyverse.org>
2018-06-12 13:57:53 -0700

    CORE-9242: use DataONE member node URLs in the resource map

--- src/data_info/services/metadata.clj
+++ src/data_info/services/metadata.clj
@@ -289,18 +289,20 @@
   [data-id {:keys [user]} {:keys [dest recursive]}]
   (metadata-save user (uuidify data-id) (ft/rm-last-slash dest) (boolean recursive)))
 
-(defn- build-ore-uri
-  "Creates a URI for an ORE aggregation from an iRODS path."
-  [path]
-  (str (curl/url (cfg/commons-base) "browse" (string/replace path #"^/+" ""))))
+(defn- ore-uri-builder
+  "Returns a function that can be used to create a URI for an ORE aggregation from an iRODS path."
+  [cm]
+  (fn [path]
+    (str (curl/url (cfg/dataone-member-node-base) "v1" "object" (irods/lookup-uuid cm path)))))
 
-(defn- build-ore [writer agg-path ore-path files avus]
-  (xml/emit (ore/to-rdf (ore/build-ore
-                         (build-ore-uri agg-path)
-                         (build-ore-uri ore-path)
-                         (mapv build-ore-uri (remove (partial = ore-path) files))
-                         avus))
-            writer))
+(defn- build-ore [cm writer agg-path ore-path files avus]
+  (let [build-ore-uri (ore-uri-builder cm)]
+    (xml/emit (ore/to-rdf (ore/build-ore
+                           (build-ore-uri agg-path)
+                           (build-ore-uri ore-path)
+                           (mapv build-ore-uri (remove (partial = ore-path) files))
+                           avus))
+              writer)))
 
 (defn- ore-save
   "Allows a data commons administrator to save an OAI-ORE file for a data set."
@@ -313,6 +315,7 @@
       (validators/path-writeable cm user path)
       (with-open [out (OutputStreamWriter. (output-stream cm ore-path))]
         (build-ore
+         cm
          out
          path
          ore-path
