f93e52e6bbd95ad21fdd306e2704444977ef8157
Ian McEwen <mian@cyverse.org>
2018-08-14 13:53:14 -0700

    Update heuristomancer for bedgz

--- project.clj
+++ project.clj
@@ -41,7 +41,7 @@
                  [org.cyverse/common-cli "2.8.1"]
                  [org.cyverse/common-cfg "2.8.1"]
                  [org.cyverse/common-swagger-api "2.8.3"]
-                 [org.cyverse/heuristomancer "2.8.5"]
+                 [org.cyverse/heuristomancer "2.8.6"]
                  [org.cyverse/kameleon "3.0.2"]
                  [org.cyverse/metadata-client "3.0.0"]
                  [org.cyverse/metadata-files "1.0.2"]
