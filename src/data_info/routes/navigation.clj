9f43abc783925a0588fce5a1feadf229a9f913d7
Paul Sarando <psarando@cyverse.org>
2019-06-14 18:18:30 -0700

    CORE-9 Migrate docs for /navigation endpoints.
    Only migrated those endpoints also exposed by terrain endpoints.

--- src/data_info/routes/navigation.clj
+++ src/data_info/routes/navigation.clj
@@ -1,6 +1,7 @@
 (ns data-info.routes.navigation
   (:use [common-swagger-api.schema]
-        [data-info.routes.schemas.common :only [get-error-code-block]])
+        [data-info.routes.schemas.common :only [get-error-code-block]]
+        [ring.util.http-response :only [ok]])
   (:require [common-swagger-api.schema.data.navigation :as schema]
             [data-info.services.directory :as dir]
             [data-info.services.root :as root]
@@ -35,31 +36,24 @@
 
     (GET "/root" [:as {uri :uri}]
       :query [{:keys [user]} StandardUserQueryParams]
-      :return schema/NavigationRootResponse
-      :summary "Root Listing"
-      :description (str
-"This endpoint provides a shortcut for the client to list the top-level directories (e.g. the user's
- home directory, trash, and shared directories)."
-(get-error-code-block
-  "ERR_DOES_NOT_EXIST, ERR_NOT_READABLE, ERR_NOT_A_USER"))
-      (svc/trap uri root/do-root-listing user))
+      :responses schema/NavigationRootResponses
+      :summary schema/NavigationRootSummary
+      :description schema/NavigationRootDocs
+      (ok (root/do-root-listing user)))
 
     (GET "/path/:zone/*" [:as {{path :*} :params uri :uri}]
       :path-params [zone :- String]
       :query [params StandardUserQueryParams]
-      :return schema/NavigationResponse
+      :responses schema/NavigationResponses
       :no-doc true
-      (svc/trap uri dir/do-directory zone path params))
+      (ok (dir/do-directory zone path params)))
 
     ;; This is actually handled by the above route, which cannot be documented properly.
     (GET "/path/:zone/:path" [:as {uri :uri}]
       :path-params [zone :- (describe String "The IRODS zone")
                     path :- (describe String "The IRODS path under the zone")]
       :query [params StandardUserQueryParams]
-      :return schema/NavigationResponse
-      :summary "Directory List (Non-Recursive)"
-      :description (str
-                     "Only lists subdirectories of the directory path passed into it."
-                     (get-error-code-block
-                       "ERR_DOES_NOT_EXIST, ERR_NOT_READABLE, ERR_NOT_A_USER, ERR_NOT_A_FOLDER"))
+      :responses schema/NavigationResponses
+      :summary schema/NavigationSummary
+      :description schema/NavigationDocs
       {:status 501})))
