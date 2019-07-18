f3b6f61b1942fe554483588e9c86df052aba3afb
Paul Sarando <psarando@cyverse.org>
2019-07-18 15:28:12 -0700

    CORE-9 Migrate existence endpoint schemas.
    Migrate schemas from data-info.routes.schemas.exists
    into common-swagger-api.schema.data.exists.
    
    Migrate data-info.routes.schemas.common/Paths
    into common-swagger-api.schema.data.

--- project.clj
+++ project.clj
@@ -50,8 +50,7 @@
                  [org.cyverse/tree-urls-client "2.8.1"]
                  [org.cyverse/event-messages "0.0.1"]
                  [com.novemberain/langohr "3.5.1"]]
-  :eastwood {:exclude-namespaces [data-info.routes.schemas.exists
-                                  data-info.routes.schemas.tickets
+  :eastwood {:exclude-namespaces [data-info.routes.schemas.tickets
                                   data-info.routes.schemas.stats
                                   data-info.routes.schemas.sharing
                                   data-info.routes.schemas.trash
--- src/data_info/routes/exists.clj
+++ src/data_info/routes/exists.clj
@@ -1,8 +1,9 @@
 (ns data-info.routes.exists
   (:use [common-swagger-api.schema]
-        [data-info.routes.schemas.common]
-        [data-info.routes.schemas.exists])
-  (:require [data-info.services.exists :as exists]
+        [common-swagger-api.schema.data :only [Paths]]
+        [data-info.routes.schemas.common :only [get-error-code-block]])
+  (:require [common-swagger-api.schema.data.exists :as schema]
+            [data-info.services.exists :as exists]
             [data-info.util.service :as svc]))
 
 
@@ -14,7 +15,7 @@
     (POST "/" [:as {uri :uri}]
       :query [params StandardUserQueryParams]
       :body [body (describe Paths "The paths to check for existence.")]
-      :return (doc-only ExistenceInfo ExistenceResponse)
+      :return (doc-only schema/ExistenceInfo schema/ExistenceResponse)
       :summary "File and Folder Existence"
       :description (str
 "This endpoint allows the caller to check for the existence of a set of files and folders."
