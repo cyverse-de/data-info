b15343a53b7c20af10376baf7abc3b83441f57fd
Ian McEwen <mian@cyverse.org>
2020-01-27 16:46:54 -0700

    Add the rest of the basic async-tasks endpoints to the client

--- src/data_info/clients/async_tasks.clj
+++ src/data_info/clients/async_tasks.clj
@@ -6,3 +6,23 @@
 (defn get-by-id
   [id]
   (async-tasks-client/get-by-id (config/async-tasks-client) id))
+
+(defn delete-by-id
+  [id]
+  (async-tasks-client/delete-by-id (config/async-tasks-client) id))
+
+(defn create-task
+  [task]
+  (async-tasks-client/create-task (config/async-tasks-client) task))
+
+(defn add-status
+  [id status]
+  (async-tasks-client/add-status (config/async-tasks-client) id status))
+
+(defn add-behavior
+  [id behavior]
+  (async-tasks-client/add-behavior (config/async-tasks-client) id behavior))
+
+(defn get-by-filter
+  [filters]
+  (async-tasks-client/get-by-filter (config/async-tasks-client) filters))
