f5d549ca020346720d1c147e6cd7a5bce4b0946d
Ian McEwen <mian@cyverse.org>
2020-03-10 10:47:08 -0700

    Use released clj-jargon 2.8.10

--- project.clj
+++ project.clj
@@ -34,7 +34,7 @@
                  [org.cyverse/clj-icat-direct "2.8.6"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
-                 [org.cyverse/clj-jargon "2.8.10-SNAPSHOT"
+                 [org.cyverse/clj-jargon "2.8.10"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
                  [org.cyverse/clojure-commons "2.8.3"]
