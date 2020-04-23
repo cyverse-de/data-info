4376ec117673fc00c31244d323079af8131c6218
Ian McEwen <mian@cyverse.org>
2020-04-22 20:22:28 -0700

    Reorganize async tasks stuff to put the thread creation/management in the client namespace

--- src/data_info/clients/async_tasks.clj
+++ src/data_info/clients/async_tasks.clj
@@ -1,5 +1,8 @@
 (ns data-info.clients.async-tasks
+  (:use [slingshot.slingshot :only [try+]])
   (:require [data-info.util.config :as config]
+            [data-info.util.irods :as irods]
+            [clojure.tools.logging :as log]
             [async-tasks-client.core :as async-tasks-client]))
 
 
@@ -30,3 +33,18 @@
 (defn get-by-filter
   [filters]
   (async-tasks-client/get-by-filter (config/async-tasks-client) filters))
+
+(defn paths-async-thread
+  [async-task-id jargon-fn]
+  (let [{:keys [username] :as async-task} (get-by-id async-task-id)
+        update-fn (fn [path action]
+                    (log/info "Updating async task:" async-task-id ":" path action)
+                    (add-status async-task-id {:status "running" :detail (format "%s: %s" path (name action))}))]
+    (try+
+      (add-status async-task-id {:status "started"})
+      (irods/with-jargon-exceptions :client-user username [cm]
+        (jargon-fn cm async-task update-fn))
+      (add-completed-status async-task-id {:status "completed"})
+      (catch Object _
+        (log/error (:throwable &throw-context) "failed processing async task" async-task-id)
+        (add-completed-status async-task-id {:status "failed"})))))
