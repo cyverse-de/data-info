38d2c9cd1c5c20e1f53aa6ff300578772e10875e
Ian McEwen <mian@cyverse.org>
2020-04-23 18:52:20 -0700

    Make a run-async-thread utility function, put in async tasks client namespace

--- src/data_info/clients/async_tasks.clj
+++ src/data_info/clients/async_tasks.clj
@@ -3,6 +3,7 @@
   (:require [data-info.util.config :as config]
             [data-info.util.irods :as irods]
             [clojure.tools.logging :as log]
+            [clojure.string :as string]
             [async-tasks-client.core :as async-tasks-client]))
 
 
@@ -33,6 +34,12 @@
 (defn get-by-filter
   [filters]
   (async-tasks-client/get-by-filter (config/async-tasks-client) filters))
+
+(defn run-async-thread
+  [async-task-id thread-function prefix]
+  (let [^Runnable task-thread (fn [] (thread-function async-task-id))]
+    (.start (Thread. task-thread (str prefix "-" (string/replace async-task-id #".*/tasks/" "")))))
+  async-task-id)
 
 (defn paths-async-thread
   [async-task-id jargon-fn]
