a373e94dccbf2e48563fdcd4c342d8443efcaa1f
Ian McEwen <mian@cyverse.org>
2020-04-28 17:04:06 -0700

    Add async-task-id to schemas for restoration

--- src/data_info/routes/schemas/trash.clj
+++ src/data_info/routes/schemas/trash.clj
@@ -17,7 +17,10 @@
   (describe RestoredFile "The restored file information.")})
 
 (s/defschema Restoration
-  {:restored (describe RestorationMap "A map of paths from the request to their restoration info")})
+  {:restored (describe RestorationMap "A map of paths from the request to their restoration info")
+
+   (s/optional-key :async-task-id)
+   (describe NonBlankString "An asynchronous task ID for the delete process being handled in the background")})
 
 ;; Used only for display as documentation in Swagger UI
 (s/defschema RestorationPathsMap
@@ -27,7 +30,10 @@
 ;; Used only for display as documentation in Swagger UI
 (s/defschema RestorationPaths
   {:restored
-   (describe RestorationPathsMap "A map of paths from the request to their restoration info")})
+   (describe RestorationPathsMap "A map of paths from the request to their restoration info")
+
+   (s/optional-key :async-task-id)
+   (describe NonBlankString "An asynchronous task ID for the delete process being handled in the background")})
 
 (s/defschema TrashPathsMap
   {(describe s/Keyword "The iRODS data item's original path.")
