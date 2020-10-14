3589fbab076ada00c8ea5a48ccc41e87355f6e3e
Ian McEwen <mian@cyverse.org>
2020-10-14 00:08:34 -0700

    Transition more things to use clj-irods, particularly metadata operations

--- src/data_info/services/manifest.clj
+++ src/data_info/services/manifest.clj
@@ -1,5 +1,6 @@
 (ns data-info.services.manifest
   (:use [data-info.services.sharing :only [anon-file-url anon-readable?]]
+        [clj-irods.core :as rods]
         [clj-jargon.metadata :only [get-attribute attribute?]]
         [slingshot.slingshot :only [try+ throw+]])
   (:require [clojure.tools.logging :as log]
@@ -9,6 +10,7 @@
             [dire.core :refer [with-pre-hook! with-post-hook!]]
             [data-info.util.validators :as validators]
             [data-info.services.stat :as stat]
+            [data-info.services.uuids :as uuids]
             [data-info.util.irods :as irods]
             [data-info.util.logging :as dul]
             [tree-urls-client.core :as tree]
@@ -24,65 +26,68 @@
       (vec tree-urls)))
 
 (defn- extract-tree-urls
-  [cm fpath]
-  (if (attribute? cm fpath (cfg/tree-urls-attr))
-    (-> (get-attribute cm fpath (cfg/tree-urls-attr))
-      first
-      :value
-      ft/basename
-      tree/get-tree-urls
-      postprocess-tree-urls)
-    []))
+  [irods user fpath]
+  (log/info (:cache irods))
+  (let [avu (rods/object-avu irods user (cfg/irods-zone) fpath {:attr (cfg/tree-urls-attr)})]
+    (if (and avu (pos? (count @avu)))
+      (-> @avu
+        first
+        :value
+        ft/basename
+        tree/get-tree-urls
+        postprocess-tree-urls)
+      [])))
 
 (defn- extract-coge-view
-  [cm fpath]
-  (if (attribute? cm fpath coge-attr)
-    (mapv (fn [{url :value} idx] {:label (str "gene_" idx) :url url})
-          (get-attribute cm fpath coge-attr) (range))
-    []))
+  [irods user fpath]
+  (let [avu (rods/object-avu irods user (cfg/irods-zone) fpath {:attr coge-attr})]
+    (if (and avu (pos? (count @avu)))
+      (mapv (fn [{url :value} idx] {:label (str "gene_" idx) :url url})
+            @avu (range))
+      [])))
 
 (defn- format-anon-files-url
   [fpath]
   {:label "anonymous" :url (anon-file-url fpath)})
 
 (defn- extract-urls
-  [cm fpath]
+  [{cm :jargon :as irods} user fpath]
   (otel/with-span [s ["extract-urls"]]
-    (let [urls (concat (extract-tree-urls cm fpath) (extract-coge-view cm fpath))]
-      (vec (if (anon-readable? cm fpath)
+    (let [urls (concat (extract-tree-urls irods user fpath) (extract-coge-view irods user fpath))]
+      (vec (if (anon-readable? @cm fpath)
              (conj urls (format-anon-files-url fpath))
              urls)))))
 
 (defn- manifest-map
-  [cm user {:keys [path] :as file}]
+  [irods user {:keys [path] :as file}]
   (-> (select-keys file [:content-type :infoType])
-      (assoc :urls (extract-urls cm path))))
+      (assoc :urls (extract-urls irods user path))))
 
 (defn- manifest
-  [{cm :jargon :as irods} user file]
-  (let [path (ft/rm-last-slash (:path file))]
-    (otel/with-span [s ["manifest validators"]]
-      (validate irods
-                [:path-exists (:path file) user (cfg/irods-zone)]
-                [:path-is-file (:path file) user (cfg/irods-zone)]
-                [:path-readable (:path file) user (cfg/irods-zone)]))
-    (manifest-map @cm user file)))
+  [{cm :jargon :as irods} user path]
+  (let [path (ft/rm-last-slash path)]
+    (validate irods
+              [:path-exists path user (cfg/irods-zone)]
+              [:path-is-file path user (cfg/irods-zone)]
+              [:path-readable path user (cfg/irods-zone)])
+    (let [file (stat/path-stat @cm user path :filter-include [:path :content-type :infoType])]
+      (manifest-map irods user file))))
 
 (defn do-manifest-uuid
   [user data-id]
   (otel/with-span [s ["do-manifest-uuid"]]
-    (irods/with-irods-exceptions nil irods
+    (irods/with-irods-exceptions {:use-icat-transaction false} irods
       (validate irods [:user-exists user (cfg/irods-zone)])
-      (let [file (stat/uuid-stat @(:jargon irods) user data-id :filter-include [:path :content-type :infoType])]
-        (manifest irods user file)))))
+      (let [path (uuids/path-for-uuid @(:jargon irods) user data-id)]
+        (manifest irods user path)))))
 
 (defn do-manifest
   [user path]
   (otel/with-span [s ["do-manifest"]]
-    (irods/with-irods-exceptions nil irods
-      (validate irods [:user-exists user (cfg/irods-zone)])
-      (let [file (stat/path-stat @(:jargon irods) user path :filter-include [:path :content-type :infoType])]
-        (manifest irods user file)))))
+    (irods/with-irods-exceptions {:use-icat-transaction false} irods
+      (validate irods
+                [:user-exists user (cfg/irods-zone)])
+        (manifest irods user path))))
 
 (with-pre-hook! #'do-manifest-uuid
   (fn [user data-id]
