6072e815c6c5ed05137f75fdd8e73465ca6fdab5
Ian McEwen <mian@cyverse.org>
2020-12-01 09:40:17 -0700

    Upgrade clj-icat-direct, clj-irods versions

--- project.clj
+++ project.clj
@@ -31,8 +31,8 @@
                  [net.sf.opencsv/opencsv "2.3"]
                  [de.ubercode.clostache/clostache "1.4.0" :exclusions [org.clojure/core.incubator]]
                  [slingshot "0.12.2"]
-                 [org.cyverse/clj-irods "0.1.3"]
-                 [org.cyverse/clj-icat-direct "2.8.11"
+                 [org.cyverse/clj-irods "0.2.0"]
+                 [org.cyverse/clj-icat-direct "2.9.0"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
                  [org.cyverse/clj-jargon "2.8.12"
