62b94435c233131dc0eb95de6d92b7b697844c87
Ian McEwen <mian@cyverse.org>
2020-12-01 09:45:05 -0700

    Increment version to new snapshot

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
       (string/trim (:out (sh "git" "rev-parse" "HEAD")))
       ""))
 
-(defproject org.cyverse/data-info "2.21.0"
+(defproject org.cyverse/data-info "2.21.1-SNAPSHOT"
   :description "provides an HTTP API for interacting with iRODS"
   :url "https://github.com/cyverse-de/data-info"
   :license {:name "BSD"
