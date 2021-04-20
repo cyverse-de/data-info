2ed4575934885a1761272ed9b9eac1a0fa4380fb
Sarah Roberts <sarahr@cyverse.org>
2021-04-19 18:44:46 -0700

    CORE-1249: migrated the /navigation/home endpoint over to new-path-stat

--- src/data_info/services/home.clj
+++ src/data_info/services/home.clj
@@ -1,25 +1,26 @@
 (ns data-info.services.home
   (:require [dire.core :refer [with-pre-hook! with-post-hook!]]
+            [clj-irods.validate :refer [validate]]
             [clj-jargon.item-info :refer [exists?]]
             [clj-jargon.item-ops :refer [mkdirs]]
             [data-info.services.stat :as stat]
+            [data-info.util.config :as cfg]
             [data-info.util.logging :as log]
             [data-info.util.irods :as irods]
-            [data-info.util.validators :as validators]
             [data-info.util.paths :as path]))
 
 (defn- user-home-path
-  [user]
+  [user zone]
   (let [user-home (path/user-home-dir user)]
-    (irods/with-jargon-exceptions [cm]
-      (validators/user-exists cm user)
-      (when-not (exists? cm user-home)
-        (mkdirs cm user-home))
-      (stat/path-stat cm user user-home :filter-include [:id :label :path :date-created :date-modified :permission]))))
+    (irods/with-irods-exceptions {} irods
+      (validate irods [:user-exists user zone])
+      (when-not (exists? @(:jargon irods) user-home)
+        (mkdirs @(:jargon irods) user-home))
+      (stat/new-path-stat irods user user-home :filter-include [:id :label :path :date-created :date-modified :permission]))))
 
 (defn do-homedir
   [{user :user}]
-  (user-home-path user))
+  (user-home-path user (cfg/irods-zone)))
 
 (with-pre-hook! #'do-homedir
   (fn [params]
