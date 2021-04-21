9fb7403b4b7df0a2d15e51c8f7e535b7a4777974
Sarah Roberts <sarahr@cyverse.org>
2021-04-21 15:25:23 -0700

    CORE-1249: updated the clj-jargon, clj-icat-direct, and clj-irods dependencies

--- project.clj
+++ project.clj
@@ -30,11 +30,11 @@
                  [javax.servlet/servlet-api "2.5"]
                  [metosin/compojure-api "1.1.13"]
                  [org.cyverse/otel "0.2.3"]
-                 [org.cyverse/clj-irods "0.2.2-SNAPSHOT"]
-                 [org.cyverse/clj-icat-direct "2.9.3-SNAPSHOT"
+                 [org.cyverse/clj-irods "0.2.2"]
+                 [org.cyverse/clj-icat-direct "2.9.3"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
-                 [org.cyverse/clj-jargon "2.8.13"
+                 [org.cyverse/clj-jargon "2.8.14"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
                  [org.cyverse/clojure-commons "3.0.6"]
