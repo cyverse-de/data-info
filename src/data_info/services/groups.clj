3213d691022d6e981719cd4214c4d9dbeaaa8f31
Ian McEwen <mian@cyverse.org>
2022-10-19 12:35:40 -0700

    Switch to use clojure.set instead of a difficult-to-read loop

--- src/data_info/services/groups.clj
+++ src/data_info/services/groups.clj
@@ -3,6 +3,7 @@
             [slingshot.slingshot :refer [throw+]]
             [clj-jargon.users :as users]
             [clojure.string :as string]
+            [clojure.set :as cset]
             [data-info.services.users :as di-users]
             [data-info.util.config :as cfg]
             [data-info.util.irods :as irods]
@@ -36,35 +37,17 @@
     (validators/user-is-group-admin cm user)
     (validators/group-exists cm group-name)
     (validators/all-users-exist cm members)
-    (let [current-members (users/list-group-members cm group-name)]
-      (loop [old-members (sort current-members)
-             new-members (sort members)]
-        ;; compare the head of both lists and determine if anything needs adding or removing
-        (let [c (first old-members)
-              n (first new-members)]
-          (cond
-            (and (nil? c) (nil? n))
-            nil ;; both lists are exhausted, we're done!
+    (let [current-members (set (users/list-group-members cm group-name))
+          desired-members (set (map di-users/ensure-qualified members))
 
-            (or (nil? c) (and
-                           (not (nil? n))
-                           (neg? (compare (di-users/qualify-username n) c)))) ;; the new member isn't in the current members, should be added
-            (do
-              (users/add-to-group cm group-name n)
-              (recur old-members (rest new-members)))
+          members-to-add (cset/difference desired-members current-members)
+          members-to-remove (cset/difference current-members desired-members)
 
-            (or (nil? n) (and
-                           (not (nil? c))
-                           (neg? (compare c (di-users/qualify-username n))))) ;; the old member isn't in the new list, should be removed
-            (do
-              (users/remove-from-group cm group-name (string/replace c (re-pattern (str "#" (:zone cm) "$")) ""))
-              (recur (rest old-members) new-members))
-
-            (= c (di-users/qualify-username n)) ;; already correct for both of these
-            (recur (rest old-members) (rest new-members))
-
-            :else
-            (throw+ {:error "Couldn't figure out what to do with users" :new n :old c}))))
+          unqualify (fn [name] (string/replace name (re-pattern (str "#\\Q" (:zone cm) "\\E$")) ""))
+          addfn (fn [user] (users/add-to-group cm group-name (unqualify user)))
+          rmfn (fn [user] (users/remove-from-group cm group-name (unqualify user)))]
+      (doall (map addfn members-to-add))
+      (doall (map rmfn members-to-remove))
       {:name group-name :members (users/list-group-members cm group-name)})))
 
 (defn delete-group
