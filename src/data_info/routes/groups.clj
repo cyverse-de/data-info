53bc9c6b0d560b58843e727f2fcde1f174d70215
Ian McEwen <mian@cyverse.org>
2022-10-19 12:42:18 -0700

    Remove extraneous str calls for descriptions

--- src/data_info/routes/groups.clj
+++ src/data_info/routes/groups.clj
@@ -18,45 +18,45 @@
     :tags ["groups"]
 
     (POST "/" []
-      :middleware [otel-middleware]
-      :query [params StandardUserQueryParams]
-      :body [body Group]
-      :responses (merge CommonResponses
-                        {200 {:schema Group
-                              :description "Successful response"}})
-      :summary "Create group"
-      :description (str "Create an IRODS group given a name and a list of members")
+      :middleware  [otel-middleware]
+      :query       [params StandardUserQueryParams]
+      :body        [body Group]
+      :responses   (merge CommonResponses
+                          {200 {:schema Group
+                                :description "Successful response"}})
+      :summary     "Create group"
+      :description "Create an IRODS group given a name and a list of members"
       (ok (groups/create-group params body)))
 
     (context ["/:group-name"] []
       :path-params [group-name :- (describe NonBlankString "The name of an iRODS group, with or without zone qualification")]
 
       (GET "/" []
-        :middleware [otel-middleware]
-        :query [params StandardUserQueryParams]
-        :responses (merge CommonResponses
-                          {200 {:schema Group
-                                :description "Successful response"}})
-        :summary "List group members"
-        :description (str "List an IRODS group & members given a name")
+        :middleware  [otel-middleware]
+        :query       [params StandardUserQueryParams]
+        :responses   (merge CommonResponses
+                            {200 {:schema Group
+                                  :description "Successful response"}})
+        :summary     "List group members"
+        :description "List an IRODS group & members given a name"
         (ok (groups/get-group params group-name)))
 
       (PUT "/" []
-        :middleware [otel-middleware]
-        :query [params StandardUserQueryParams]
-        :body [body GroupMembers]
-        :responses (merge CommonResponses
-                          {200 {:schema Group
-                                :description "Successful response"}})
-        :summary "Update group members"
-        :description (str "Update an IRODS group's members")
+        :middleware  [otel-middleware]
+        :query       [params StandardUserQueryParams]
+        :body        [body GroupMembers]
+        :responses   (merge CommonResponses
+                            {200 {:schema Group
+                                  :description "Successful response"}})
+        :summary     "Update group members"
+        :description "Update an IRODS group's members"
         (ok (groups/update-group-members params body group-name)))
 
       (DELETE "/" []
-        :middleware [otel-middleware]
-        :query [params StandardUserQueryParams]
-        :responses (merge CommonResponses
-                          {200 {:description "Successful response"}})
-        :summary "Delete group"
-        :description (str "Delete an IRODS group's members")
+        :middleware  [otel-middleware]
+        :query       [params StandardUserQueryParams]
+        :responses   (merge CommonResponses
+                            {200 {:description "Successful response"}})
+        :summary     "Delete group"
+        :description "Delete an IRODS group's members"
         (ok (groups/delete-group params group-name))))))
