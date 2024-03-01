bbcb795826538a09375e1f1c829801980c6cf7cb
Sarah Roberts <sarahr@cyverse.org>
2024-03-01 15:44:59 -0700

    DD-18: updated dependencies

--- project.clj
+++ project.clj
@@ -14,8 +14,8 @@
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :manifest {"Git-Ref" ~(git-ref)}
   :uberjar-name "data-info-standalone.jar"
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [cheshire "5.10.0"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [cheshire "5.12.0"
                    :exclusions [[com.fasterxml.jackson.dataformat/jackson-dataformat-cbor]
                                 [com.fasterxml.jackson.dataformat/jackson-dataformat-smile]
                                 [com.fasterxml.jackson.core/jackson-annotations]
@@ -24,7 +24,7 @@
                  [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
                  [dire "0.5.4"]
                  [me.raynes/fs "1.4.6"]
-                 [org.apache.tika/tika-core "1.26"]
+                 [org.apache.tika/tika-core "2.9.1"]
                  [net.sf.opencsv/opencsv "2.3"]
                  [de.ubercode.clostache/clostache "1.4.0" :exclusions [org.clojure/core.incubator]]
                  [org.cyverse/clj-irods "0.3.4"]
@@ -36,17 +36,17 @@
                                 [log4j]]]
                  [javax.servlet/servlet-api "2.5"]
                  [metosin/compojure-api "1.1.13"]
-                 [ring/ring-jetty-adapter "1.6.3"] ;; update this when underlying ring version changes, probably
+                 [ring/ring-jetty-adapter "1.11.0"] ;; update this when underlying ring version changes, probably
                  [org.cyverse/otel "0.2.5"]
                  [org.cyverse/clojure-commons "3.0.7"]
                  [org.cyverse/common-cli "2.8.1"]
                  [org.cyverse/common-cfg "2.8.2"]
-                 [org.cyverse/common-swagger-api "3.1.0"]
+                 [org.cyverse/common-swagger-api "3.4.4"]
                  [org.cyverse/heuristomancer "2.8.6"]
-                 [org.cyverse/kameleon "3.0.6"]
+                 [org.cyverse/kameleon "3.0.8"]
                  [org.cyverse/metadata-client "3.1.1"]
                  [org.cyverse/async-tasks-client "0.0.4"]
-                 [org.cyverse/metadata-files "1.0.3"]
+                 [org.cyverse/metadata-files "2.1.0"]
                  [org.cyverse/oai-ore "1.0.3"]
                  [org.cyverse/service-logging "2.8.3"]
                  [org.cyverse/event-messages "0.0.1"]
@@ -58,10 +58,12 @@
                                   data-info.routes.schemas.trash
                                   :test-paths]
              :linters [:wrong-arity :wrong-ns-form :wrong-pre-post :wrong-tag :misplaced-docstrings]}
-  :plugins [[test2junit "1.1.3"]
-            [jonase/eastwood "0.4.0"]]
+  :plugins [[jonase/eastwood "1.4.2"]
+            [lein-ancient "0.7.0"]
+            [test2junit "1.1.3"]]
   :profiles {:dev     {:plugins        [[lein-ring "0.12.5"]]
-                       :resource-paths ["conf/test"]}
+                       :resource-paths ["conf/test"]
+                       :jvm-opts       ["-Dotel.javaagent.enabled=false"]}
              :repl    {:source-paths ["repl"]}
              :uberjar {:aot :all}}
   :main ^:skip-aot data-info.core
